9da5a09dea9b527474d274154497bea324956026
akshudata69-jpg <akshudata69@gmail.com>
2026-03-21 07:44:46 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,29 +3,34 @@ import sqlite3
 def init_db():
     conn = sqlite3.connect('safedeal.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, username TEXT, score INTEGER DEFAULT 50, status TEXT DEFAULT '🟡 Safe')''')
-    c.execute('''CREATE TABLE IF NOT EXISTS sellers (username TEXT PRIMARY KEY, deals TEXT, channel TEXT, experience TEXT)''')
+    # Users for broadcast
+    c.execute('CREATE TABLE IF NOT EXISTS users (user_id PRIMARY KEY)')
+    # Scammers with Proofs
+    c.execute('CREATE TABLE IF NOT EXISTS scammers (username TEXT PRIMARY KEY, amount TEXT, proof_id TEXT)')
+    # Verified Sellers
+    c.execute('CREATE TABLE IF NOT EXISTS sellers (username TEXT PRIMARY KEY, channel TEXT, deals TEXT)')
     conn.commit()
     conn.close()
 
-def add_user(uid, user):
+def add_to_db(table, data):
     conn = sqlite3.connect('safedeal.db')
     c = conn.cursor()
-    c.execute("INSERT OR IGNORE INTO users (user_id, username) VALUES (?, ?)", (uid, user))
+    if table == "users":
+        c.execute("INSERT OR IGNORE INTO users VALUES (?)", (data,))
+    elif table == "scammers":
+        c.execute("INSERT OR REPLACE INTO scammers VALUES (?, ?, ?)", data)
+    elif table == "sellers":
+        c.execute("INSERT OR REPLACE INTO sellers VALUES (?, ?, ?)", data)
     conn.commit()
     conn.close()
 
-def get_all_uids():
+def check_user(username):
+    username = username.replace("@", "").strip().lower()
     conn = sqlite3.connect('safedeal.db')
     c = conn.cursor()
-    c.execute("SELECT user_id FROM users")
-    data = [row[0] for row in c.fetchall()]
+    c.execute("SELECT * FROM scammers WHERE LOWER(username)=?", (username,))
+    scam = c.fetchone()
+    c.execute("SELECT * FROM sellers WHERE LOWER(username)=?", (username,))
+    sell = c.fetchone()
     conn.close()
-    return data
-
-def save_reg(user, deals, channel, exp):
-    conn = sqlite3.connect('safedeal.db')
-    c = conn.cursor()
-    c.execute("INSERT OR REPLACE INTO sellers VALUES (?, ?, ?, ?)", (user, deals, channel, exp))
-    conn.commit()
-    conn.close()
+    return scam, sell
